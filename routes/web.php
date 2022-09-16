e8d5661c40ce2f84b3cd32862f16bd0410b1b781
cedric KAKANAKOU <cedric.kakanakou@pivotech.xyz>
2022-09-16 13:57:32 +0100

    route name corrected

--- routes/web.php
+++ routes/web.php
@@ -32,10 +32,10 @@ Route::get('/contact', function() {
     return 'Contact';
 });
 
-Route::get('/domaines', [App\Http\Controllers\DomaineController::class, 'index'])->name('domain_list');
+Route::get('/domaines', [App\Http\Controllers\DomaineController::class, 'index'])->name('domain.list');
 
-Route::get('/domaines/add', [App\Http\Controllers\DomaineController::class, 'create'])->name('domain_create');
+Route::get('/domaines/add', [App\Http\Controllers\DomaineController::class, 'create'])->name('domain.create');
 
-Route::get('/domaines/show', [App\Http\Controllers\DomaineController::class, 'show'])->name('domain_show');
+Route::get('/domaines/show', [App\Http\Controllers\DomaineController::class, 'show'])->name('domain.show');
 
-Route::get('/domaines/edit', [App\Http\Controllers\DomaineController::class, 'edit'])->name('domain_edit');
+Route::get('/domaines/edit', [App\Http\Controllers\DomaineController::class, 'edit'])->name('domain.edit');
