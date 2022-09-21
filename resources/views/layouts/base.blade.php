1f349154851f87b2c1bd4778344498edc38a233a
ezechiel.bambelou <ezechiel.bambelou@pivotech.xyz>
2022-09-21 18:54:53 +0100

    clipboad feature added and domain check validation on input added

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -101,6 +101,7 @@
 <!--begin::Global Javascript Bundle(used by all pages)-->
 <script src="{{asset('assets/plugins/global/plugins.bundle.js')}}"></script>
 <script src="{{asset('assets/js/scripts.bundle.js')}}"></script>
+
 <!--end::Global Javascript Bundle-->
 <!--begin::Vendors Javascript(used by this page)-->
 <script src="{{asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.js')}}"></script>
