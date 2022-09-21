e0320a46e2c953ef85764e1293f3ff10114bedeb
cedric KAKANAKOU <cedric.kakanakou@pivotech.xyz>
2022-09-21 15:56:10 +0100

    btn copie start

--- resources/views/back/domains/index.blade.php
+++ resources/views/back/domains/index.blade.php
@@ -18,7 +18,7 @@
                     <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
                         <!--begin::Item-->
                         <li class="breadcrumb-item text-muted">
-                            <a href="index.html" class="text-muted text-hover-primary">Home</a>
+                            <a href="{{ route('home') }}" class="text-muted text-hover-primary">Home</a>
                         </li>
                         <!--end::Item-->
                         <!--begin::Item-->
@@ -86,9 +86,9 @@
                                 <!--begin::Table head-->
                                 <thead>
                                     <tr class="fw-bold text-muted bg-light">
-                                        <th class="ps-4 min-w-400px rounded-start">Domain Name</th>
-                                        <th class="min-w-425px">Api Key</th>
-                                        <th class="min-w-300px text-end rounded-end">Actions </th>
+                                        <th class="ps-4 min-w-325px rounded-start">Domain Name</th>
+                                        <th class="min-w-125px">Api Key</th>
+                                        <th class="min-w-150px text-center">Actions</th>
                                     </tr>
                                 </thead>
                                 <!--end::Table head-->
@@ -99,19 +99,44 @@
                                         <td>
                                             <div class="d-flex align-items-center">
                                                 <div class="symbol symbol-50px me-5">
-{{--                                                    <img src="../assets/media/stock/600x400/img-26.jpg" class="" alt="" />--}}
+                                                    <a href="{{ $domaine->name }}"  target="_blank" class="text-dark fw-bold text-hover-primary mb-1 fs-6">
+                                                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr064.svg-->
+                                                        <span class="svg-icon svg-icon-2">
+                                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                <rect opacity="0.5" x="18" y="13" width="13" height="2" rx="1" transform="rotate(-180 18 13)" fill="currentColor" />
+                                                                <path d="M15.4343 12.5657L11.25 16.75C10.8358 17.1642 10.8358 17.8358 11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25L18.2929 12.7071C18.6834 12.3166 18.6834 11.6834 18.2929 11.2929L12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75C10.8358 6.16421 10.8358 6.83579 11.25 7.25L15.4343 11.4343C15.7467 11.7467 15.7467 12.2533 15.4343 12.5657Z" fill="currentColor" />
+                                                            </svg>
+                                                        </span>
+                                                        <!--end::Svg Icon-->
+                                                    </a>
                                                 </div>
                                                 <div class="d-flex justify-content-start flex-column">
-                                                    <a href="#" class="text-dark fw-bold text-hover-primary mb-1 fs-6">{{ $domaine->name }}</a>
-                                                    <span class="text-muted fw-semibold text-muted d-block fs-7"></span>
+
+                                                    <span class="text-dark fw-bold mb-1 fs-6">{{ $domaine->name }}</span>
                                                 </div>
                                             </div>
                                         </td>
                                         <td>
-                                            <a href="#" class="text-dark fw-bold text-hover-primary d-block mb-1 fs-6">{{ $domaine->apikey  }}</a>
-                                            <span class="text-muted fw-semibold text-muted d-block fs-7"></span>
+                                            <div class="d-flex align-items-center">
+                                                <div class="symbol symbol-50px me-5">
+                                                    <a href="{{ $domaine->name }}"  target="_blank" class="text-dark fw-bold text-hover-primary mb-1 fs-6">
+                                                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr064.svg-->
+                                                        <span class="svg-icon svg-icon-2">
+                                                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                    <rect opacity="0.5" x="18" y="13" width="13" height="2" rx="1" transform="rotate(-180 18 13)" fill="currentColor" />
+                                                                    <path d="M15.4343 12.5657L11.25 16.75C10.8358 17.1642 10.8358 17.8358 11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25L18.2929 12.7071C18.6834 12.3166 18.6834 11.6834 18.2929 11.2929L12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75C10.8358 6.16421 10.8358 6.83579 11.25 7.25L15.4343 11.4343C15.7467 11.7467 15.7467 12.2533 15.4343 12.5657Z" fill="currentColor" />
+                                                                </svg>
+                                                            </span>
+                                                        <!--end::Svg Icon-->
+                                                    </a>
+                                                </div>
+                                                <div class="d-flex justify-content-start flex-column">
+                                                    <span class="text-dark fw-bold  d-block mb-1 fs-6">{{ $domaine->apikey  }}</span>
+
+                                                </div>
+                                            </div>
                                         </td>
-                                        <td class="text-end">
+                                        <td class="text-center">
                                             <a href="{{ route("domain.edit", $domaine) }}" class="btn btn-icon btn-bg-light btn-active-color-warning  me-1">
                                                 <!--begin::Svg Icon | path: icons/duotune/art/art005.svg-->
                                                 <span class="svg-icon svg-icon-3">
@@ -146,6 +171,7 @@
 {{--                                            </form>--}}
                                             <!-- Formulaire pour supprimer un Post : "posts.destroy" -->
                                         </td>
+
                                     </tr>
                                 @endforeach
 
