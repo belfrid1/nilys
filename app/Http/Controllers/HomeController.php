7f320df9ba1572146e04c8afff8052de18cb673d
ezechiel.bambelou <ezechiel.bambelou@pivotech.xyz>
2022-10-02 17:11:55 +0100

    dashboard update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
+use App\Models\Domain;
+use App\Models\Popup;
+use App\Models\PopupGroup;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,6 +29,34 @@ class HomeController extends Controller
     public function index()
     {
 
-        return view('back.dashboard');
+
+
+        $domainLastAddCount = count(Domain::where('created_at', '>=', Carbon::now()->subdays(15))->get());
+        $domainLastUpdateCount = count(Domain::where('updated_at', '>=', Carbon::now()->subdays(15))->get());
+        $data["domainsCount"] = count(Domain::all());
+        $data["domainsLastUpdateCount"] = $domainLastUpdateCount;
+        $data["domainsLastAddCount"] = $domainLastAddCount;
+
+        $contactLastAddCount = count(Contact::where('created_at', '>=', Carbon::now()->subdays(15))->get());
+        $contactLastUpdateCount = count(Contact::where('updated_at', '>=', Carbon::now()->subdays(15))->get());
+        $data["contactsLastUpdateCount"] = $contactLastUpdateCount;
+        $data["contactsLastAddCount"] = $contactLastAddCount;
+        $data["contactsCount"] = count(Contact::all());
+
+
+        $groupLastAddCount = count(PopupGroup::where('created_at', '>=', Carbon::now()->subdays(15))->get());
+        $groupLastUpdateCount = count(PopupGroup::where('updated_at', '>=', Carbon::now()->subdays(15))->get());
+        $data["groupsCount"] = count(PopupGroup::all());
+        $data["groupsLastUpdateCount"] = $groupLastUpdateCount;
+        $data["groupsLastAddCount"] = $groupLastAddCount;
+
+        $popupLastAddCount = count(Popup::where('created_at', '>=', Carbon::now()->subdays(15))->get());
+        $popupLastUpdateCount = count(Popup::where('updated_at', '>=', Carbon::now()->subdays(15))->get());
+        $data["popupsCount"] = count(Popup::all());
+        $data["popupsLastUpdateCount"] = $popupLastUpdateCount;
+        $data["popupsLastAddCount"] = $popupLastAddCount;
+
+
+        return view('back.dashboard', compact(['data']));
     }
 }
